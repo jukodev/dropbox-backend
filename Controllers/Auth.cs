86290f89d1f25f3642f0c2c4035b4b2f6b885170
65Juko <thejulianjuko@gmail.com>
2023-03-19 22:39:01 +0100

    Add project files.

---
+++ Controllers/Auth.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace drop_grungus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auth : ControllerBase
+    {
+        [HttpPost("/login")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Login([FromBody] string username, [FromBody] string password) {
+            return NotFound();
+        }
+
+        [HttpPost("/validate")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Login([FromBody] string token)
+        {
+            return NotFound();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace drop_grungus.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+}
