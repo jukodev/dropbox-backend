c0b2a9cbe7b69171a5d9721ce07b7d3f7ce157ab
Julian <thejulianjuko@gmail.com>
2023-03-23 16:11:47 +0100

    added base sample logun

--- Controllers/Auth.cs
+++ Controllers/Auth.cs
@@ -8,10 +8,27 @@ namespace drop_grungus.Controllers
     [ApiController]
     public class Auth : ControllerBase
     {
+        private User[] validUsers = { new User("valid", "valid", "vtoken") };
+
         [HttpPost("/login")]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult Login([FromBody] User user) {
-            return NotFound();
+            if(user == null)
+            {
+                return BadRequest();
+            }
+            
+            var _user = validUsers.FirstOrDefault(u => u.Name.Equals(user.Name) && u.Password.Equals(user.Password));
+            // TODO check for valid login
+            if(_user == default) {
+                return Unauthorized();
+            }
+
+            return NoContent();
+
+
         }
 
         [HttpPost("/validate")]
--- Models/User.cs
+++ Models/User.cs
@@ -6,10 +6,26 @@ namespace drop_grungus.Models
     {
         [Required]
         [MaxLength(15)]
+        [MinLength(3)]
         public string Name { get; set; }
 
         [Required]
         [MaxLength(255)]
+        [MinLength(6)]
         public string Password { get; set; }
+
+        public string Token { get; set; }
+
+        public User(String name, String password) {
+            Name = name;
+            Password = password;
+        }
+
+        public User(string name, string password, string token)
+        {
+            Name = name;
+            Password = password;
+            Token = token;
+        }
     }
 }
