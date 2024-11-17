252ed54e22c3c2a4001caf9baa387363a26e1aa6
Julian <thejulianjuko@gmail.com>
2024-11-17 15:05:08 +0100

    added base aspnet 8 project with razor pages

---
+++ burger-supply-backend/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace burger_supply_backend.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
